59abe47661ca6f594cc00822b529634d5e4eace6
ChrisDoykov <krisdoikov@gmail.com>
2022-04-05 13:48:15 +0100

    added comments

--- receiveMessage.js
+++ receiveMessage.js
@@ -1,5 +1,5 @@
 /*
-  Auhtor: Kristiyan Doykov
+  Author: Kristiyan Doykov
   Student No.: 170780410
   Purpose: The following script displays the information about the IOTA node which
   the device is connected to and retrieves, decrypts (using AES) and parses all
@@ -10,28 +10,40 @@
 
 const { ClientBuilder } = require('@iota/client');
 const CryptoJS = require('crypto-js');
+
+// Import ENV variables
 const { NODE, SENSOR_DATA_MESSAGE_INDEX, DATA_SECRET } = process.env;
 
 const client = new ClientBuilder().node(NODE).build();
 
+// Get info about the node
 client.getInfo().then(console.log).catch(console.error);
 
 const getMessagesByIndex = async (index) => {
+  // Get the IDs of all messages under the given index
   const messages = await client.getMessage().index(index);
 
+  // Process each message individually
   for (let message of messages) {
+    // Get the response data
     const res = await client.getMessage().data(message);
+    // Convert from buffer to a UTF8 string
     const data = Buffer.from(res.message.payload.data, 'hex').toString('utf8');
 
+    // If able to decrypt using AES(the original encryptio method) and
+    // the original secret -> Log the decrypted data
     try {
       const bytes = CryptoJS.AES.decrypt(data, DATA_SECRET);
       const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
       console.log('Data: ', decryptedData);
     } catch (e) {
       try {
+        // If data is in plain JSON format -> Log it directly
         console.log('Data: ', JSON.parse(data));
       } catch (e) {
-        console.log('Unrecognized hash: ', data);
+        // If AES is not the encryption method and the data is not
+        // in plain JSON
+        console.log('Unrecognized encryption: ', data);
       }
     }
   }
--- sendMessage.js
+++ sendMessage.js
@@ -1,5 +1,5 @@
 /*
-  Auhtor: Kristiyan Doykov
+  Author: Kristiyan Doykov
   Student No.: 170780410
   Purpose: The following script displays the information about the IOTA node which
   the device is connected to and sends AES-encrypted data coming from the DHT11 
@@ -8,31 +8,36 @@
   Last Updated: 01 April 2022 
  */
 
+const { ClientBuilder } = require('@iota/client');
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
-const moment = require('moment');
 const CryptoJS = require('crypto-js');
+const moment = require('moment');
 
-const { ClientBuilder } = require('@iota/client');
+// Import ENV variables
 const { NODE, ARDUINO_SERIAL_PORT, SENSOR_DATA_MESSAGE_INDEX, DATA_SECRET } =
   process.env;
 
 const client = new ClientBuilder().node(NODE).build();
 
+// Get info about the node
 client.getInfo().then(console.log).catch(console.error);
 
+// Initialize the serial connection with the Arduino
 const serial = new SerialPort({
   baudRate: 9600,
   autoOpen: true,
   path: ARDUINO_SERIAL_PORT,
 });
 
+// Parse serial input
 const parser = serial.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-// Publish to tangle
+// Publish to the Tangle
 const publish = async (packet) => {
   const stringPayload = JSON.stringify(packet);
 
+  // Encrypt the output data
   const ciphertext = CryptoJS.AES.encrypt(
     stringPayload,
     DATA_SECRET
@@ -40,6 +45,7 @@ const publish = async (packet) => {
 
   const MessageSender = client.message();
 
+  // Attach the message to the given index and retrieve the messageId
   const messageId = await MessageSender.index(SENSOR_DATA_MESSAGE_INDEX)
     .data(ciphertext)
     .submit();
@@ -47,7 +53,7 @@ const publish = async (packet) => {
   console.log('Message ID: ', messageId);
 };
 
-// Serial port library events
+// Serial port events
 serial.on('open', showPortOpen);
 parser.on('data', readSerialData);
 serial.on('close', showPortClose);
@@ -58,9 +64,11 @@ function showPortOpen() {
   console.log('Serial port open. Data rate: ' + serial.baudRate + '\n');
 }
 
+// Whenever new data is read
 async function readSerialData(data) {
   console.log('Read serial data.\n');
 
+  // Process the data and transform into JSON format
   let json = {};
 
   const dateTime = moment().utc().format('DD/MM/YYYY hh:mm:ss');
@@ -69,6 +77,7 @@ async function readSerialData(data) {
   json['temperature'] = data.split(',')[0].split(':')[1].trim();
   json['humidity'] = data.split(',')[1].split(':')[1].trim();
 
+  // Publish the message
   publish(json);
 
   console.log('json = ', json);
